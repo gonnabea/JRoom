5a4f2633e2dcb20504a9a4ceb59a1441147ef253
Choi Jiwon <winning1993@naver.com>
2021-01-13 22:25:30 +0900

    [REFACTOR] addDirLight, addSpotLight 모듈화

--- src/Components/ThreeScene.tsx
+++ src/Components/ThreeScene.tsx
@@ -3,17 +3,7 @@ import * as THREE from "three"
 import styled from "styled-components"
 import { OrbitControls } from "three-orbitcontrols-ts"
 import { GodRaysEffect, RenderPass, EffectPass, EffectComposer } from "postprocessing"
-import {
-  AmbientLight,
-  BackSide,
-  Color,
-  CullFaceFront,
-  DoubleSide,
-  FlatShading,
-  Shape,
-  ShapePath,
-} from "three"
-import floorImage from "../resources/images/floor1.jpg"
+import { DoubleSide } from "three"
 import floorImage2 from "../resources/images/floor2.jpg"
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader"
 import { CSG } from "three-csg-ts"
@@ -51,46 +41,49 @@ const ThreeScene = () => {
     const ExhibitionRoom = new THREE.Mesh(buildingGeometry, buildingMaterial)
     ExhibitionRoom.position.set(0, 0, -3000)
 
-    // mesh 내부에서도 면이 보이게 만들어 줌.
-    ExhibitionRoom.material.side = THREE.BackSide
-    // scene.add( ExhibitionRoom );
+    ExhibitionRoom.material.side = THREE.BackSide // mesh 내부에서도 면이 보이게 만들어 줌.
+
     const ambientLight = new THREE.AmbientLight(0xc2cee9, 0.7) // soft white light
     ambientLight.position.set(0, 6000, 0)
     scene.add(ambientLight)
 
     // 디렉셔널 라이트 (햇빛)
-    const dirLight = new THREE.DirectionalLight(0xffffff, 0.3)
-    dirLight.position.set(-1000, 2000, 2000)
-    dirLight.castShadow = true
 
-    const targetObject = new THREE.Object3D()
-    scene.add(targetObject)
-    dirLight.target = targetObject
-    dirLight.target.position.set(-500, 1000, 800)
-    dirLight.target.updateMatrixWorld()
+    const addDirLight = ([x, y, z]: Array<number>, [x1, y1, z1]: Array<number>) => {
+      const dirLight = new THREE.DirectionalLight(0xffffff, 0.3)
+      dirLight.position.set(x, y, z)
+      dirLight.castShadow = true
 
-    const dirLightHelper = new THREE.DirectionalLightHelper(dirLight, 300, 0xa0c2f9)
-    dirLightHelper.update()
+      dirLight.target.position.set(x1, y1, z1)
+      dirLight.target.updateMatrixWorld()
 
-    scene.add(dirLight, dirLight.target)
-    scene.add(dirLightHelper)
+      const dirLightHelper = new THREE.DirectionalLightHelper(dirLight, 300, 0xa0c2f9)
+      dirLightHelper.update()
+
+      scene.add(dirLight, dirLight.target)
+      scene.add(dirLightHelper)
+    }
+    addDirLight([-1000, 2000, 2000], [-500, 1000, 800])
 
     // 스포트라이트 (창문 통과하는 햇빛)
-    const spotLight_distance = 0 // 빛의 최대범위
-    const spotLight_angle = Math.PI / 18
-    const spotLight = new THREE.SpotLight(0xffffff, 0.9, spotLight_distance, spotLight_angle)
-    spotLight.penumbra = 1
-    spotLight.decay = 0.5
-    // 창문 위치
-    spotLight.position.set(-900, 750, 800)
-    spotLight.target = targetObject
-    spotLight.target.position.set(-400, -500, -100)
-    spotLight.target.updateMatrixWorld()
 
-    const spotLightHelper = new THREE.SpotLightHelper(spotLight)
-    scene.add(spotLightHelper)
+    const addSpotLight = ([x, y, z]: Array<number>, [x1, y1, z1]: Array<number>, angle: number) => {
+      const spotLight_distance = 0 // 빛의 최대범위
+      const spotLight = new THREE.SpotLight(0xffffff, 0.9, spotLight_distance, angle)
+      spotLight.penumbra = 1
+      spotLight.decay = 0.5
+      // 창문 위치
+      spotLight.position.set(x, y, z)
+      spotLight.target.position.set(x1, y1, z1)
+      spotLight.target.updateMatrixWorld()
 
-    scene.add(spotLight)
+      const spotLightHelper = new THREE.SpotLightHelper(spotLight)
+      scene.add(spotLightHelper)
+
+      scene.add(spotLight)
+    }
+
+    addSpotLight([-900, 750, 800], [-400, -500, -100], Math.PI / 18)
 
     //// 프로젝트 방 (Just-Read-It) ////
 
