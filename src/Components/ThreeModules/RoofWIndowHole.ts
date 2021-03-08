f75e64cfbbec0c331e1ebc527930ff6e34321567
Choi Jiwon <winning1993@naver.com>
2021-03-09 01:02:09 +0900

    [FIX] geometry.faces 프로퍼티를 ts 에서 인식하지 못해 코드 수정

--- src/Components/ThreeModules/RoofWIndowHole.ts
+++ src/Components/ThreeModules/RoofWIndowHole.ts
@@ -51,8 +51,7 @@ export const addRoofWindowHole = (
   const bspMeshResult = CSG.toMesh(bspResult, roofMesh.matrix)
 
   bspMeshResult.material = roofMesh.material
-
-  bspMeshResult.geometry.faces.splice(97, 40) // face 목록 중 가장 끝의 것들만 제거하면 패인 부분을 제거할 수 있음
+  ;(bspMeshResult as any).geometry.faces.splice(97, 40) // face 목록 중 가장 끝의 것들만 제거하면 패인 부분을 제거할 수 있음
 
   // 두번째 창문 구멍 생성
 
@@ -62,8 +61,7 @@ export const addRoofWindowHole = (
   const bspResult2 = bspRoof2.subtract(bspWindowHole2)
   const bspMeshResult2 = CSG.toMesh(bspResult2, bspMeshResult.matrix)
   bspMeshResult2.material = roofMesh.material
-
-  bspMeshResult2.geometry.faces.splice(97, 16)
+  ;(bspMeshResult2 as any).geometry.faces.splice(97, 16)
 
   // 세번쨰 창문 구멍 생성
 
@@ -73,8 +71,7 @@ export const addRoofWindowHole = (
   const bspResult3 = bspRoof3.subtract(bspWindowHole3)
   const bspMeshResult3 = CSG.toMesh(bspResult3, bspMeshResult.matrix)
   bspMeshResult3.material = roofMesh.material
-
-  bspMeshResult3.geometry.faces.splice(97, 18)
+  ;(bspMeshResult3 as any).geometry.faces.splice(97, 18)
 
   bspMeshResult3.rotateZ(Math.PI / 2)
   bspMeshResult3.rotateX(Math.PI / 2)
