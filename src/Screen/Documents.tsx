7765e45786d8d1e51771a8890fc11dcd31c33976
Choi Jiwon <winning1993@naver.com>
2021-03-14 05:26:28 +0900

    [ADD] Documents (CS 백과 사이드바)

--- src/Screen/Documents.tsx
+++ src/Screen/Documents.tsx
@@ -1,5 +1,42 @@
+import styled from "styled-components"
+
+const Container = styled.main``
+
+const WikiLogo = styled.img`
+  width: 120px;
+  height: 120px;
+`
+
+const Header = styled.header`
+  display: flex;
+`
+
+const SideBar = styled.aside`
+  padding: 20px;
+  background-color: #f6f6f6;
+  height: 100vh;
+  width: 10em;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+const PageTitle = styled.h1`
+  font-size: 18px;
+`
+
+const TitleTail = styled.span``
+
 const Documents: React.FC = () => {
-  return <div>"Documents"</div>
+  return (
+    <Container>
+      <SideBar>
+        <WikiLogo src="images/wiki_logo.png"></WikiLogo>
+        <PageTitle>위키백과</PageTitle>
+        <TitleTail>Jiwon의 CS 백과</TitleTail>
+      </SideBar>
+    </Container>
+  )
 }
 
 export default Documents
