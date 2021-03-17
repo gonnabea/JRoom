727623126e1c31d7579170617796f8e1bf7c7ec4
Choi Jiwon <winning1993@naver.com>
2021-03-17 22:59:31 +0900

    [FIX] 책 닫혔을 때 페이지 넘겨지는 버그 해결 -> Books 페이지 완성

--- src/Components/Books/3D-Book.tsx
+++ src/Components/Books/3D-Book.tsx
@@ -133,7 +133,7 @@ const Inside1 = styled.div`
   width: calc(${(props: props) => (props.width ? props.width : "100px")} + (-10px));
   height: calc(${(props) => (props.height ? props.height : "150px")} + (-10px));
   transform: translateX(5px)
-    translateZ(${(props) => (props.width ? `calc(${props.width} / -20)` : "-5px")});
+    translateZ(${(props) => (props.width ? `calc(${props.width} / -18)` : "-5px")});
   background-color: white;
   border-radius: 0 3px 3px 0;
   color: black;
@@ -154,7 +154,7 @@ const Inside2 = styled.div`
   width: calc(${(props: props) => (props.width ? props.width : "100px")} + (-10px));
   height: calc(${(props) => (props.height ? props.height : "150px")} + (-10px));
   transform: translateX(5px)
-    translateZ(${(props) => (props.width ? `calc(${props.width} / -18)` : "-5px")});
+    translateZ(${(props) => (props.width ? `calc(${props.width} / -16)` : "-5px")});
   background-color: white;
   border-radius: 0 3px 3px 0;
 `
@@ -167,7 +167,7 @@ const Inside3 = styled.div`
   width: calc(${(props: props) => (props.width ? props.width : "100px")} + (-10px));
   height: calc(${(props) => (props.height ? props.height : "150px")} + (-10px));
   transform: translateX(5px)
-    translateZ(${(props) => (props.width ? `calc(${props.width} / -16)` : "-5px")});
+    translateZ(${(props) => (props.width ? `calc(${props.width} / -14)` : "-5px")});
   background-color: white;
   border-radius: 0 3px 3px 0;
 `
@@ -180,7 +180,7 @@ const Inside4 = styled.div`
   width: calc(${(props: props) => (props.width ? props.width : "100px")} + (-10px));
   height: calc(${(props) => (props.height ? props.height : "150px")} + (-10px));
   transform: translateX(5px)
-    translateZ(${(props) => (props.width ? `calc(${props.width} / -14)` : "-5px")});
+    translateZ(${(props) => (props.width ? `calc(${props.width} / -12)` : "-5px")});
   background-color: white;
   border-radius: 0 3px 3px 0;
 `
@@ -192,7 +192,7 @@ const Inside5 = styled.div`
   width: calc(${(props: props) => (props.width ? props.width : "100px")} + (-10px));
   height: calc(${(props) => (props.height ? props.height : "150px")} + (-10px));
   transform: translateX(5px)
-    translateZ(${(props) => (props.width ? `calc(${props.width} / -12)` : "-5px")});
+    translateZ(${(props) => (props.width ? `calc(${props.width} / -10)` : "-5px")});
   background-color: white;
   border-radius: 0 3px 3px 0;
 `
@@ -204,7 +204,7 @@ const Inside6 = styled.div`
   width: calc(${(props: props) => (props.width ? props.width : "100px")} + (-10px));
   height: calc(${(props) => (props.height ? props.height : "150px")} + (-10px));
   transform: translateX(5px)
-    translateZ(${(props) => (props.width ? `calc(${props.width} / -10)` : "-5px")});
+    translateZ(${(props) => (props.width ? `calc(${props.width} / -8)` : "-5px")});
   background-color: white;
   border-radius: 0 3px 3px 0;
 `
@@ -223,40 +223,51 @@ const Book3D: React.FC<props> = ({
   spine,
   bookTitle,
 }) => {
+  // 책 닫혔을 때 책 페이지 넘겨지는 버그 방지
+  let bookState: string
+
+  // 책 꺼내기
   const takeOutBook = (e: any) => {
-    // 더 좋은 알고리즘 필요.
+    e.stopPropagation()
 
     e.currentTarget.style.animation = "takeOut 0.5s forwards"
     e.currentTarget.style.zIndex = 1
   }
 
+  // 책 돌려놓기
   const revertBook = (e: any) => {
-    e.currentTarget.style.animation = null // js로 트리거되는 애니메이션을 없애서 css 애니메이션을 적용
-    e.currentTarget.childNodes[0].style.animation = "" // 책 제 자리에 놓기
+    bookState = "close"
+    e.currentTarget.style.animation = null // js로 트리거되는 애니메이션을 없애서 css 애니메이션을 발동시킴
+    e.currentTarget.childNodes[0].style.animation = "" // 모든 element를 원래대로
 
     for (let i = 0; i < e.currentTarget.childNodes.length; i++) {
       e.currentTarget.childNodes[i].style.animation = ""
       e.currentTarget.childNodes[i].style.transform = ""
     }
+
+    // zIndex를 변경하여 자연스런 효과 유도
     setTimeout(
       (e: any) => {
         if (e) {
           e.style.zIndex = 0
         }
       },
-      300,
+      200,
       e.currentTarget
     )
   }
 
   const openBook = (e: any) => {
     console.dir(e.currentTarget)
+    bookState = "open"
     e.currentTarget.style.animation = "openBook 0.5s forwards"
     e.currentTarget.style.background = null
   }
 
   const flipPage = (e: any) => {
-    e.currentTarget.style.animation = "flipPage 0.5s forwards"
+    if (bookState === "open") {
+      e.currentTarget.style.animation = "flipPage 0.5s forwards"
+    }
   }
 
   const closeBook = (e: any) => {
